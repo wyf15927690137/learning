01cb281c0b830d5444f0f28317f7d6b747d5e44e
yanfeiw <yanfeiw@cadence.com>
2022-08-02 00:31:45 +0800

    1

--- learnbat/7stringcontain.bat
+++ learnbat/7stringcontain.bat
@@ -1,4 +1,26 @@
+@echo off
 set str=machine-order-service
 set matchStr=order
+@REM not show the output of a command
+@REM > nul
+
+echo "abc" 
+echo "abc" > nul
+echo "-----------------------"
+
 echo %str% | findstr %matchStr% >nul && echo yes || echo no
+
+@REM tzuitl /g : get time zone
+@REM command1 && command2 : command2 will be executed only when command1 successfully executed
+@REM command1 || command2 : command2 will be executed only when command1 failed to execute
 tzutil /g | findstr "China" >nul && (echo yes) || (echo no)
+
+
+set str1=abc
+set str2=abc123
+@REM findstr will return the whole line which contains the found content
+@REM find str1 in str2
+echo %str2% | findstr %str1%
+echo "-----------------------"
+echo "wy" | findstr "w"
+echo "-----------------------"
--- learnpy/27system.py
+++ learnpy/27system.py
@@ -1,12 +1,28 @@
 import os
+# os.system execute the operation system command
 os.system("echo this is a test")
 os.system("dir")
 os.system("echo %WORKSPACE%")
+# get current file path
 cwd=os.getcwd()
 print(cwd)
 
-# r prefix in python
+# r prefix in python : all string will be treated as a raw string
 print('input\n')
 print(r'input\n')
 
-os.listdir("D:\PersonalLearn\learning")
+# f prefix in python : get the value of a variable
+name = 'wyf'
+print(f'{name}')
+print("================")
+
+# get the files list of a folder
+dic = os.listdir("D:\PersonalLearn\learning")
+print(dic)
+print("================")
+
+# travel all the files of a folder
+for path,dirs,files in os.walk("D:\PersonalLearn\learning\learnbat"):
+    print(path)
+    print(dirs)
+    print(files)
