d44faa58881cd21f3687520f53f01a868fec839b
yanfeiw <yanfeiw@cadence.com>
2022-09-13 12:43:44 +0800

    a

--- learnsh/6.exitcode.sh
+++ learnsh/6.exitcode.sh
@@ -1,6 +1,13 @@
-#!/bin/sh
-echo test
-# set exit code as $?, if last command executes successfully, then $? == 0, and then the current script will be exited.
-exit $?
+#!/bin/csh
+echo "this is csh"
 
-echo test1
+if ( "true" == "true" ) then
+    echo "success"
+    # set exit code as $?, if last command executes successfully, then $? == 0, and then the current script will be exited.
+    exit $?
+else
+    echo "fail"
+    ./code.sh
+    exit $?
+endif
+exit $?
